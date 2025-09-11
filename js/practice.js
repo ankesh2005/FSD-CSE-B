c8d8661eb54b23d9b2deba676fa8f2044936d6dd
ankesh2005 <ankeshgupta1802@gmail.com>
2025-09-11 15:34:52 +0530

    call back hell

--- js/practice.js
+++ js/practice.js
@@ -1,20 +1,47 @@
-function register(){
-  setTimeout(()=>{
-    console.log("register end");
-  },2000)
-}
- function login(){
-  setTimeout(()=>{
-    console.log("login");
-  },2000)
- }
 
- function getdata(){
+// 💥 Callback Hell Example in JavaScript
+
+function register(cb) {
   setTimeout(() => {
-    console.log("fetching");
-  }, 200);
- }
- 
-register();
-login();
-getdata();
+    console.log("User registered");
+    cb();
+  }, 1000);
+}
+
+function sendEmail(cb) {
+  setTimeout(() => {
+    console.log("Email sent");
+    cb();
+  }, 1000);
+}
+
+function login(cb) {
+  setTimeout(() => {
+    console.log("User logged in");
+    cb();
+  }, 1000);
+}
+
+function getData(cb) {
+  setTimeout(() => {
+    console.log("Data retrieved");
+    cb();
+  }, 1000);
+}
+
+function displayData() {
+  setTimeout(() => {
+    console.log("Data displayed");
+  }, 1000);
+}
+
+// ⚠️ Callback Hell in action
+register(() => {
+  sendEmail(() => {
+    login(() => {
+      getData(() => {
+        displayData();
+      });
+    });
+  });
+});
