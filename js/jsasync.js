fa324a6c6dd7a5c9e34cafd084dd104ebce184d8
ankesh2005 <ankeshgupta1802@gmail.com>
2025-09-15 14:11:36 +0530

    learning synchronus and asynchrouns

---
+++ js/jsasync.js
@@ -0,0 +1,20 @@
+function register(){
+  console.log('register end');
+}
+function sentemail(){
+  console.log('email send email');
+}
+function login(){
+  console.log('login end');
+}
+function getdata(){
+  console.log('get data');
+}
+function displayData(){
+  console.log('display data');
+}
+register();
+sentemail();
+login();
+getdata();
+displayData();
